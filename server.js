8e14dbe5c5bb2eff5f1253a04ba29b79af6c825b
Luis Hernandez <luishrd@gmail.com>
2018-03-22 13:07:42 -0600

    updates packages and moves to yarn

--- server.js
+++ server.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const cors = require('cors');
 const port = 3333;
 
 const server = express();
-server.use(bodyParser.json());
+server.use(express.json());
 server.use(cors());
 
 const sendUserError = (msg, res) => {
