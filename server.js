0f199b480af4fb980c2eeddbcad7516dc51b3a7e
Henry Blevins <heblevi@gmail.com>
2019-04-12 13:23:36 -0400

    Add id to default smurf, increment smurfId

--- server.js
+++ server.js
@@ -16,13 +16,14 @@ let smurfs = [
   {
     name: 'Brainey',
     age: 200,
-    height: '5cm'
+    height: '5cm',
+    id: 0
   }
 ];
 server.get('/smurfs', (req, res) => {
   res.json(smurfs);
 });
-let smurfId = 0;
+let smurfId = 1;
 
 server.post('/smurfs', (req, res) => {
   const { name, age, height } = req.body;
