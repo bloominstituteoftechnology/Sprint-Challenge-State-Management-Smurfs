4332e3701a899e57ced971b0380a392312c98680
Wes Jonke <wesjonke@gmail.com>
2019-07-26 12:01:43 -0400

    added basic file structure

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,9 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import thunk from 'redux-thunk';
-import {reducer} from './store/reducer/index';
-
+import {reducer} from './store/reducers/index';
+import thunk from 'redux-thunk'
 
 import "./index.css";
 import App from "./components/App";
