8f214a45e080b0f4528a8e790e2a26cd3b685c80
dangolbeeker <scottb305@outlook.com>
2019-08-16 11:46:47 -0400

    smurfs/smurf components added

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Smurf.css';
+
+ class Smurf extends Component {
+  render() {
+    return (
+      <h2>SMURFS!</h2>
+     
+    )
+  }
+}
+
+ export default Smurf;
---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Smurf from './Smurf';
+
+ const Smurfs = props => {
+  return (
+      <h1>Smurfs!!</h1>
+  )
+}
+
+ export default Smurfs;
