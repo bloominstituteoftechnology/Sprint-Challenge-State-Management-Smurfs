1ea140a97803d2da47a2e21a9b173878e7bd00ec
dangolbeeker <scottb305@outlook.com>
2019-08-16 11:24:03 -0400

    setting up smurf fetch action

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,5 @@
+import axios from 'axios';
+export const FETCH_SMURF_START = "FETCH_SMURF_START";
+export const FETCH_SMURF_SUCCESS = "FETCH_SMURF_SUCCESS";
+export const FETCH_SMURF_FAILURE = "FETCH_SMURF_FAILURE";
+
