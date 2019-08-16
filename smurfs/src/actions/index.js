9b736696b27f0b28e33ef41b5b7d460847f4f910
dangolbeeker <scottb305@outlook.com>
2019-08-16 12:36:59 -0400

    styling/working on actions/reducers actions completed

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -3,3 +3,17 @@ import axios from 'axios';
 export const GET_SMURFS = "GET_SMURFS";
 export const GET_SMURFS_SUCCESS = "GET_SMURFS_SUCCESS";
 export const GET_SMURFS_FAILURE = "GET_SMURFS_FAILURE";
+
+
+export const getSmurfs = () => dispatch => {
+    dispatch({ type: GET_SMURFS})
+    axios.get('http://localhost:3333/smurfs')
+    .then(res => {
+        console.log(res);
+        dispatch({ type: GET_SMURFS_SUCCESS, payload: res.data })
+    })
+    .catch(err => {
+        console.log(err)
+        dispatch({ type: GET_SMURFS_FAILURE, payload: err.message })
+    })
+}
--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,22 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Smurfs from './Smurfs';
+import Navigation from './Navigation';
+import SmurfForm from './SmurfForm';
 
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navigation />
+          <Route path="/" component={Smurfs} />
+          <Route path="/smurf-form" component={SmurfForm} />
+        </div>
+      </Router>
+    );
+  }
+}
 
+export default App;
