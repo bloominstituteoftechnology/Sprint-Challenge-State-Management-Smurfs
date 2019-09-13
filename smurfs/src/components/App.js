2a31dc00205a488c16739687c91458f3d470d4f6
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-13 09:35:54 -0700

    provide smurf context to App with smurfs reducer

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,20 @@
-import React, { Component } from "react";
-import "./App.css";
-class App extends Component {
-  render() {
-    return (
+import React, { useReducer } from "react"
+
+import SmurfCtx from '../contexts/SmurfCtx'
+import smurfReducer from '../reducers/smurfReducer'
+import "./App.css"
+
+export default function App () {
+  const [smurfs, dispatch] = useReducer(smurfReducer, [])
+
+  return (
+    <SmurfCtx.Provider value={{smurfs, dispatch}}>
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
       </div>
-    );
-  }
+    </SmurfCtx.Provider>
+  )
 }
-
-export default App;
