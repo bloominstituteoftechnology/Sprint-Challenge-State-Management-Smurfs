a4d49beb9bd1f66f2a19e636ce17ccddf28899e8
Moses A. Samuel <mosamuel98@gmail.com>
2019-09-13 10:51:27 -0700

    write action + reducer logic for adding new smurfs

--- smurfs/src/store/actions/index.js
+++ smurfs/src/store/actions/index.js
@@ -1,19 +1,45 @@
+const baseUrl = "http://localhost:3333"
+
 export const GET_SMURFS_START = "GET_SMURFS_START"
 export const GET_SMURFS_SUCCESS = "GET_SMURFS_SUCCESS"
 export const GET_SMURFS_FAILURE = "GET_SMURFS_FAILURE"
 
-export const getSmurfs = () => (dispatch) => {
+export const getSmurfs = () => async dispatch => {
     dispatch({ type: GET_SMURFS_START })
 
-    fetch("http://localhost:3333/smurfs")
-    .then(res => res.json())
-    .then(parsedRes => {
-        dispatch({ type: GET_SMURFS_SUCCESS, payload: parsedRes })
-    })
-    .catch(res => {
+    try {
+        const res = await fetch(`${baseUrl}/smurfs`)
+        const parsedRes = await res.json()
+        dispatch({ type: GET_SMURFS_SUCCESS, payload: parsedRes})
+    } catch(e) {
         dispatch({ type: GET_SMURFS_FAILURE })
-        console.error(res)
-    })
+        console.error(e)
+    }
 }
 
+
+export const ADD_NEW_SMURF_START = "ADD_NEW_SMURF_START"
+export const ADD_NEW_SMURF_SUCCESS = "ADD_NEW_SMURF_SUCCESS"
+export const ADD_NEW_SMURF_FAILURE = "ADD_NEW_SMURF_FAILURE"
+
+export const addNewSmurf = smurf => async dispatch => {
+    dispatch({ type: ADD_NEW_SMURF_START })
+
+    try {
+        dispatch({ type: ADD_NEW_SMURF_START })
+        const res = await fetch(`${baseUrl}/smurfs`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(smurf)
+        })
+        const parsedRes = await res.json()
+        dispatch({ type: ADD_NEW_SMURF_SUCCESS, payload: parsedRes })
+    } catch(e) {
+        dispatch({ type: ADD_NEW_SMURF_FAILURE })
+        console.error(e)
+    }
+    
+}
 
--- smurfs/src/store/reducers/index.js
+++ smurfs/src/store/reducers/index.js
@@ -1,7 +1,11 @@
 import { 
     GET_SMURFS_START,
     GET_SMURFS_SUCCESS, 
-    GET_SMURFS_FAILURE 
+    GET_SMURFS_FAILURE,
+
+    ADD_NEW_SMURF_START,
+    ADD_NEW_SMURF_SUCCESS,
+    ADD_NEW_SMURF_FAILURE
 } from '../actions'
 
 const initialState = {
@@ -25,6 +29,23 @@ export default function smurfReducer(state=initialState, action) {
             }
 
         case GET_SMURFS_FAILURE: 
+            return {
+                ...state,
+                fetching: false
+            }
+
+        case ADD_NEW_SMURF_START: 
+            return {
+                ...state, 
+                fetching: true
+            }
+        case ADD_NEW_SMURF_SUCCESS: 
+            return {
+                ...state,
+                fetching: false,
+                smurfs: state.smurfs.concat(action.payload.slice(-1))
+            }
+        case ADD_NEW_SMURF_FAILURE: 
             return {
                 ...state,
                 fetching: false
