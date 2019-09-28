42974ae5812165a27e4b7f6d3f2feb32ac62bef1
AidenBow <AidenBow@users.noreply.github.com>
2019-09-27 18:46:59 -0700

    imported thunk

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -5,9 +5,10 @@ import App from "./components/App";
 import {createStore, applyMiddleware} from "redux"
 import {Provider} from "react-redux"
 import {reducer} from "./reducers/reducers"
+import thunk from "redux-thunk"
 
 
-const store = createStore(reducer, applyMiddleware())
+const store = createStore(reducer, applyMiddleware(thunk))
 
 ReactDOM.render(
     <Provider store={store}>
