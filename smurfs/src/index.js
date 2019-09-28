8356c7fa8fbc191284f910e3971a6fed18bc36be
Gilbert <keron.gilbert@igt.com>
2019-09-27 21:59:46 -0400

    Initial commit

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from "react"
+import ReactDOM from "react-dom"
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"
+import { composeWithDevTools } from "redux-devtools-extension"
+import reduxThunk from "redux-thunk"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import App from "./components/App"
+import "./index.css"
+
+import rootReducer from "./store/reducers"
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(reduxThunk))
+)
+
+ReactDOM.render(
+  <Provider store={store}>
+      <App />
+  </Provider>,
+  document.getElementById("root")
+)
