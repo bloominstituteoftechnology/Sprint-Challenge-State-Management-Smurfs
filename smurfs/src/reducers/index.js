f6afcd097475154edb1fc44ff076d1e0f4e24898
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-09-27 21:20:04 -0400

    Set up the redux store and reducer

---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,15 @@
+const initialState = {
+  name: "",
+  age: 0,
+  height: "",
+  id: 0
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducer;
