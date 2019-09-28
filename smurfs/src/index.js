fb0ac8e7b537fb347caee8e1a396785c03dd5579
AidenBow <AidenBow@users.noreply.github.com>
2019-09-27 18:21:28 -0700

    created store, actions, and reducers

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {createStore, applyMiddleware} from "redux"
+import {Provider} from "react-redux"
+import {reducer} from "./reducers/reducers"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const store = createStore(reducer, applyMiddleware)
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root"));
---
+++ smurfs/src/reducers/reducers.js
@@ -0,0 +1,5 @@
+
+export const reducer = (state, action) => {
+
+    return state
+}
