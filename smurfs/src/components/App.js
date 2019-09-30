4d1bae86cfe3f43b66eb9a9d0198cd1be74f47db
JHaydenDev <34730927+JHaydenDev@users.noreply.github.com>
2019-09-30 14:05:20 -0500

    Set up context and get request.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,35 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from "axios";
+import Context from "./Context";
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: []
+    };
+  }
+  componentDidMount() {
+    axios
+      .get("http://localhost:3333")
+      .then(res => {
+        this.setState({ data: res.data });
+      })
+      .catch(error => {
+        console.log(error, "the smurf has been eaten");
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <Context.Provider>
+          <h1>SMURFS! 2.0 W/ Redux</h1>
+          <div>Welcome to your state management version of Smurfs!</div>
+          <div>Start inside of your `src/index.js` file!</div>
+          <div>Have fun!</div>
+        </Context.Provider>
       </div>
     );
   }
