1006c620d9a73c5e19ca2e9ac8741fdae62cae64
lizdoyle <lizdoyle1121@gmail.com>
2019-09-29 21:29:29 -0400

    add redux

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import {applyMiddleware, createStore} from "redux";
+import {Provider} from "react-redux";
 import App from "./components/App";
+import logger from "redux-logger";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById("root"));
