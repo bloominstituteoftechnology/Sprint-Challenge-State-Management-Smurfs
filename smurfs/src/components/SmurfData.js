b1795d87ab3baa62e16dcacaec97fc8812102487
Jacqueline Omollo <jacquelineOmolloo@gmail.com>
2019-09-29 23:37:16 -0400

    Commit with bugs

--- smurfs/src/components/SmurfData.js
+++ smurfs/src/components/SmurfData.js
@@ -1,3 +1,22 @@
-import React, { useEffect, useContext } from "react";
-import { SmurfContext } from "../contexts/SmurfContext";
-import axios from "axios";
+// import React from "react";
+// import { SmurfContext } from "./contexts/SmurfContext";
+// import { useContext } from "react";
+
+// const SmurfData = () => {
+//   const { smurfs } = useContext(SmurfContext);
+
+//   return (
+//     <div>
+//       {smurfs.map(data => (
+//         <div>
+//           <p>{data.name}</p>
+//           <p>{data.age}</p>
+//           <p>{data.height}</p>
+//           <p>{data.id}</p>
+//         </div>
+//       ))}
+//     </div>
+//   );
+// };
+
+// export default SmurfData;
