2dfb96157abcf7eb55670636f5c964f1aa2ef7ac
Chazaray Robinson <chaarayrobinson@Chazarays-MacBook-Pro.local>
2019-10-11 14:18:50 -0400

    MVP

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,5 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore, applyMiddleware } from "redux"
+import { Provider } from "react-redux"
+import { reducer } from "./reducers/reducers"
+import thunk from "redux-thunk"
+
+const store = createStore(reducer, applyMiddleware(thunk))
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById("root"));
