b2af2d4ba8721bfe2899eb174fef9686af4f9168
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-11 10:18:23 +0100

    create new actionCreator and smurf Reducer

---
+++ smurfs/src/state/actionCreator.js
@@ -0,0 +1,10 @@
+import * as types from './actionTypes';
+
+export const getSmurf = feature => {
+    return {type: types.GET_SMURF}
+}
+
+export const addNewSmurf = feature => {
+    return {type: types.ADD_NEW_SMURF}
+}
+
--- smurfs/src/state/reducers.js
+++ smurfs/src/state/reducers.js
@@ -8,3 +8,16 @@ const initialState = [
         "id": 0
     }
 ]
+
+const smurfReducer = (state=initialState, action) => {
+    switch(action.type){
+        case types.ADD_NEW_SMURF:
+            return {
+                ...state
+            }
+
+        default:
+            return state;
+
+    }
+}
