a363996f9d1b7de2cfa2fffc0110685f1a8cdc27
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-11 09:50:46 +0100

    Add barebone form component for creating new smurf

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import Smurf from './Smurf';
+import Form from './Form';
 import "./App.css";
 class App extends Component {
   render() {
@@ -8,6 +10,8 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <Smurf />
+        <Form />
       </div>
     );
   }
---
+++ smurfs/src/components/Form.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Form() {
+
+
+    return(
+        <div>
+            <p>Create New Smurf here</p>
+            <p>Name: <input /></p>
+            <p>Age: <input /></p>
+            <p>Height: <input /></p>
+        </div>
+    )
+}
+
+export default Form
