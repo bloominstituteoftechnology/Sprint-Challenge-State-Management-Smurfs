f510035b7e362219fd14e5e44c6303dbf490820f
Mike Nguyen <mpvnguyen1996@gmail.com>
2019-10-11 13:45:19 -0400

    added styling

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,17 @@
 import React, { Component } from "react";
 import "./App.css";
 import SmurfList from "./SmurfList";
-import AddSmurfForm from './SmurfForm';
+import SmurfForm from './SmurfForm';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
+        {/* <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-        <AddSmurfForm />
+        <div>Have fun!</div> */}
+        <SmurfForm />
         <SmurfList />
       </div>
     );
