f8a856e40a69b9328ee764f236ca9227abb32a75
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-12 00:14:59 +0100

    modify <Smurf /> component to render name, height and age details of individual smurfs

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+import * as actionCreators from "../state/actionCreator";
 
-function Smurf(props) {
+export function Smurf(props) {
+  const { getSmurf, smurfData } = props;
+  useEffect(() => {
+    getSmurf();
+  }, []);
 
-
-    return(
-        <div>
-            This is our smurf component
-        </div>
-    )
+  return (
+    <div>
+      {smurfData ? smurfData.map(smurf => {
+        return (
+          <div key={smurf.id}>
+            <p>{smurf.name}</p>
+            <p>{smurf.age}</p>
+            <p>{smurf.height}</p>
+          </div>
+        );
+      })
+      : null}
+    </div>
+  );
 }
 
-export default Smurf;
+export default connect(
+  state => state,
+  actionCreators
+)(Smurf);
