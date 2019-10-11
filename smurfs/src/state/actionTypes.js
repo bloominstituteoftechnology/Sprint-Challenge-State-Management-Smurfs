8f87cd1705f4a66fafaae84ae2118d25500f5417
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-11 10:08:13 +0100

    create slice of state and the actionTypes our slice of state will suffer

---
+++ smurfs/src/state/actionTypes.js
@@ -0,0 +1,3 @@
+
+export const ADD_NEW_SMURF = "ADD_NEW_SMURF";
+export const GET_SMURF = "GET_SMURF";
---
+++ smurfs/src/state/reducers.js
@@ -0,0 +1,10 @@
+import * as types from './actionTypes';
+
+const initialState = [
+    {
+        "name": "Brainey",
+        "age": 200,
+        "height": "5cm",
+        "id": 0
+    }
+]
