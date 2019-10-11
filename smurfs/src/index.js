b3c640dad53e120443e2f6af6d1c62162f4053b3
Temitope Akinsoto <takinsoto@gmail.com>
2019-10-12 00:17:11 +0100

    use combineReducers to create a combined reducer and inject the monster reducer into the store using createStor utitlity

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,31 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import thunk from "redux-thunk";
+
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import * as reducers from "./state/reducers";
+import { Provider } from "react-redux";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export const monsterReducer = combineReducers({
+  smurfData: reducers.smurfReducer,
+  formInputValues: reducers.formReducer
+});
+
+export const store = createStore(
+  monsterReducer,
+  {},
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
