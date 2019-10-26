f9fb204b17b67e7c2b3c704f9517a02cc684bf5d
Dennis Padiernos <depadiernos@gmail.com>
2019-10-25 20:27:53 -0500

    style add smurf form to match

--- smurfs/src/components/AddSmurfs.js
+++ smurfs/src/components/AddSmurfs.js
@@ -1,11 +1,33 @@
 import React, { useState, useContext } from "react";
+import styled from "styled-components";
 import { SmurfContext } from "../contexts/SmurfContext";
+
+const Style = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  border-radius: 1rem;
+  padding: 1rem;
+  margin: 1rem auto;
+  background: rgb(0, 0, 0, 0.4);
+  color: white;
+
+  input {
+    padding: 0.5rem;
+  }
+  button {
+    background: white;
+    border: none;
+    border-radius: 0.5rem;
+    padding: 0.5rem;
+  }
+`;
 
 export default function AddSmurfs() {
   const [newSmurf, setNewSmurf] = useState({
     name: "",
-    age: '',
-    height: ''
+    age: "",
+    height: ""
   });
   const { addSmurfs } = useContext(SmurfContext);
 
@@ -14,43 +36,41 @@ export default function AddSmurfs() {
   };
 
   const handleSubmit = e => {
-      e.preventDefault()
-      addSmurfs(newSmurf)
-  }
+    e.preventDefault();
+    addSmurfs(newSmurf);
+  };
 
   return (
-    <form onSubmit={(e)=>handleSubmit(e)}>
-      <h2>Add a smurf.</h2>
-      <label>
-        Name:{" "}
-        <input
-          type="text"
-          name="name"
-          value={newSmurf.name}
-          onChange={e => handleChange(e)}
-        />
-      </label>
+    <Style onSubmit={e => handleSubmit(e)}>
+      <h2>Add a smurf</h2>
+
+      <input
+        type="text"
+        name="name"
+        placeholder="Name"
+        value={newSmurf.name}
+        onChange={e => handleChange(e)}
+      />
       <br />
-      <label>
-        Age:{" "}
-        <input
-          type="number"
-          name="age"
-          value={newSmurf.age}
-          onChange={e => handleChange(e)}
-        />
-      </label>
+
+      <input
+        type="number"
+        name="age"
+        placeholder="Age"
+        value={newSmurf.age}
+        onChange={e => handleChange(e)}
+      />
       <br />
-      <label>
-        Height:{" "}
-        <input
-          type="number"
-          name="height"
-          value={newSmurf.height}
-          onChange={e => handleChange(e)}
-        />
-      </label><br/>
+
+      <input
+        type="number"
+        name="height"
+        placeholder="Height"
+        value={newSmurf.height}
+        onChange={e => handleChange(e)}
+      />
+      <br />
       <button>Add</button>
-    </form>
+    </Style>
   );
 }
