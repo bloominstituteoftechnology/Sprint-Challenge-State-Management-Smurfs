cd1be9e44e009d24a56e5b284e3449a99b4c2053
lesley <lesleybanadzem2017@gmail.com>
2019-10-26 11:15:21 -0700

    able to delete

--- smurfs/src/components/SmurfsCards/SmurfCard.jsx
+++ smurfs/src/components/SmurfsCards/SmurfCard.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
 import styled from 'styled-components';
+import axios from 'axios'
 import smurfImg from './download.jpeg'
 function SmurfCard(props) {
+    console.log(props.smurf)
+    function deleteSmurf(id){
+        axios.delete(`http://localhost:3333/smurfs/${id}`)
+        .then(res=> console.log(res))
+        .catch(err=> console.log(err))
+    }
     return (
         <CardStyles>
         <div className='header'>
@@ -11,6 +18,11 @@ function SmurfCard(props) {
             <h3> Name: {props.smurf.name}</h3>
             <h4>Age: {props.smurf.age}</h4>
             <h5>Height: {props.smurf.height}</h5>
+            <p className='delete' 
+                onClick={e=>{
+                    deleteSmurf(props.smurf.id)
+                }}
+            >Delete</p>
         </div>
         </CardStyles>
             
@@ -20,18 +32,25 @@ function SmurfCard(props) {
 export default SmurfCard
 
 const CardStyles = styled.div`
-height: 300px;
+height: 400px;
 width: 250px;
 background: lightcoral;
 margin: 10px 5px; 
 
 .header{
-height: 55%;
+height: 50%;
 }
 img{
     width: 100%;
 }
 .card-body{
     padding: 10px 0px;
+}
+.delete{
+    float: right;
+    right: 0;
+    margin: 0px 20px 0px 0px;
+    cursor: pointer;
+
 }
 `;
