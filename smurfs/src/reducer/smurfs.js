5d3eb104e8ea341918ae289a573a87097aaddcfc
lesley <lesleybanadzem2017@gmail.com>
2019-10-26 08:15:01 -0700

    created switch cases in the reduce to handle data fetching

--- smurfs/src/reducer/smurfs.js
+++ smurfs/src/reducer/smurfs.js
@@ -1,3 +1,4 @@
+import { FETCH_SMURFS_SUCCESS, FETCH_SMURFS_FETCHING, FETCH_SMURFS_ERROR } from './../actions/smurfs'
 const intState = {
     error: null,
     data: null,
@@ -5,5 +6,24 @@ const intState = {
 }
 
 export function reducer(state= intState, action){
+    switch(action.type){
+        case FETCH_SMURFS_FETCHING:
+            return {
+                ...state,
+                isLoading: true
+            }
+        case FETCH_SMURFS_SUCCESS:
+            return {
+                ...state,
+                data: action.payload,
+                isLoading: false
+            }
+        case FETCH_SMURFS_FETCHING:
+            return {
+                ...state,
+                data: action.payload,
+                isLoading: false
+            }
+    }
     return state
 }
