4bed4faa35a5ec77be95d50d63d7fa2bed4e8d5f
lesley <lesleybanadzem2017@gmail.com>
2019-10-26 08:09:40 -0700

    remove constructor

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,7 @@
 import React, { Component } from "react";
-import axios from 'axios'
 import "./App.css";
 class App extends Component {
-  constructor(props){
-    super(props);
 
-  }
-  componentDidMount(){
-    axios.get('http://localhost:3333/smurfs')
-      .then(res=>console.log(res))
-      .catch(err=>console.log(err))
-  }
   render() {
     return (
       <div className="App">
