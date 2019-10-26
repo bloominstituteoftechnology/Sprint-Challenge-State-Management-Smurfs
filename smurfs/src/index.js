8d0e9be5ded69dd9ae67302a197a89d26c24e1d9
lesley <lesleybanadzem2017@gmail.com>
2019-10-26 07:48:46 -0700

    createsd a store and passed in the reducer and looger to test redux store was working

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider} from 'react-redux';
+import logger from 'redux-logger';
+
 import "./index.css";
+import { reducer } from './reducer/smurfs'
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const root = combineReducers({reducer})
+const store = createStore(root, applyMiddleware(logger));
+
+ReactDOM.render(
+    <Provider store = { store }>
+        <App />
+    </Provider>
+    , document.getElementById("root"));
