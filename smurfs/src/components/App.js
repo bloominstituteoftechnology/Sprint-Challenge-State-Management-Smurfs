fb6d7590367493b5eaac6ff2c2f27297fe68ae32
Dennis Padiernos <depadiernos@gmail.com>
2019-10-25 19:36:49 -0500

    implement context api

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,39 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import { SmurfContext } from "../contexts/SmurfContext"
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
+import SmurfsList from './SmurfsList'
+import AddSmurfs from "./AddSmurfs";
+
+function App() {
+  const [smurfs, setSmurfs] = useState()
+
+  useEffect(() => {
+    axios.get("http://localhost:3333/smurfs")
+      .then((res) => {
+        setSmurfs([...res.data])
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  const addSmurfs = (smurf) =>{
+    axios.post("http://localhost:3333/smurfs", smurf)
+    .then((res) => {
+      console.log(res.data)
+    })
+    .catch((err) => console.log(err))
+  }
+
+  return (
+    <SmurfContext.Provider value={{smurfs, setSmurfs, addSmurfs}}>
       <div className="App">
         <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <SmurfsList/>
+        <AddSmurfs/>
       </div>
-    );
-  }
+    </SmurfContext.Provider>
+  );
+
 }
 
 export default App;
