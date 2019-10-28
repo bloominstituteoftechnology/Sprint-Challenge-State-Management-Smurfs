2775d6b3fa76667a58db5cb7b4df3fa8bc483460
Andrew Nottoli <nottolivc@gmail.com>
2019-10-28 02:02:40 +0000

    Delete function stretch problem

--- smurfs/src/components/Form.jsx
+++ smurfs/src/components/Form.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
-import {addSmurf, getSmurfs} from '../store/actions/index';
+import {addSmurf, getSmurfs, deleteSmurf} from '../store/actions/index';
 import uuid from 'react-uuid';
+
 const Form = props => {
 
 const [smurf, setSmurf] = useState({name:'', age:'', height:''});
@@ -25,7 +26,12 @@ const FormSubmit = event =>{
     setSmurf({name:'', age:'', height:''})
 };
 
- 
+const handleDelete = (event) => {
+  event.preventDefault()
+  deleteSmurf(event.target.value)
+}
+
+
 return (
 
     <form onSubmit={FormSubmit} className='villageForm'>
@@ -42,7 +48,7 @@ return (
 
     <label>Smurf Age: </label>
     <input
-      type="number"
+      type="text"
       name="age"
       placeholder="Smurfs Age"
       value={smurf.age}
@@ -53,14 +59,14 @@ return (
       <input
         type="text"
         name="height"
-        placeholder="Sumurfs Height"
+        placeholder="Smurfs Height"
         value={smurf.height} 
         onChange={handleChange}
       />
     
     
     <button type="submit">Add Smurf</button>
-
+    <button onClick={handleDelete} value={smurf.id}>Delete Smurf</button>
   </form>
 );
 };
