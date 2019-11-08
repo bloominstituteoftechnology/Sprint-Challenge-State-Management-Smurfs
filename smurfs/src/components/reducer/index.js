e7e5d825d8bf869afc313002b253d429417ab33d
Drazen <danstad2012@gmail.com>
2019-11-08 07:50:24 -0800

    install dependencies

---
+++ smurfs/src/components/reducer/index.js
@@ -0,0 +1,41 @@
+// import {
+//   FETCH_POKEMON_LOADING,
+//   FETCH_POKEMON_SUCCESS,
+//   FETCH_POKEMON_FAILED
+// } from "../actions";
+
+const initialState = {
+  pokemon: [],
+  error: null,
+  isFetching: false
+};
+
+function reducer(state = initialState, action) {
+  console.log("reducer", action);
+  switch (action.type) {
+    // case FETCH_POKEMON_LOADING:
+    //   return {
+    //     ...state,
+    //     isFetching: true,
+    //     error: null
+    //   };
+    // case FETCH_POKEMON_SUCCESS:
+    //   return {
+    //     ...state,
+    //     pokemon: action.payload,
+    //     isFetching: false,
+    //     error: null
+    //   };
+    // case FETCH_POKEMON_FAILED:
+    //   return {
+    //     ...state,
+    //     pokemon: [],
+    //     isFetching: false,
+    //     error: action.payload
+    //   };
+    default:
+      return state;
+  }
+}
+
+export default reducer;
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import reducer from "./components/reducer";
+import { composeWithDevTools } from "redux-devtools-extension";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk, logger))
+);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
