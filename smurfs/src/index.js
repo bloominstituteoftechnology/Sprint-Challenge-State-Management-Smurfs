565c3b99fb9cabfdeb1cdd476564bad8c736a89c
Sara De La Cruz <saradelacruz@Saras-MacBook-Air.local>
2019-11-08 10:46:58 -0600

    set up basic structure

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from 'redux';
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import { reducer}  from "./reducers/reducer";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk, logger)));
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
---
+++ smurfs/src/reducers/reducer.js
@@ -0,0 +1,8 @@
+const initialState = [];
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+} 
