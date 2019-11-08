cae4edae64787b8733a599d83e8e2547f3a98a5d
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-08 13:15:10 -0500

    trying to set up SmirfVillage.js

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
+import SmurfVillage from "./SmurfVillage";
 
 
 class App extends Component {
@@ -10,6 +11,7 @@ class App extends Component {
         <div>Welcome to your state management version of Smurfs!</div>
         <div>Start inside of your `src/index.js` file!</div>
         <div>Have fun!</div>
+        <SmurfVillage/>
       </div>
     );
   }
--- smurfs/src/components/SmurfVillage.js
+++ smurfs/src/components/SmurfVillage.js
@@ -1,6 +1,15 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux"; 
+
 import { fetchSmurfs } from "../actions/actions";
 import { addSmurf } from "../actions/actions";
 import Smurfs from "./Smurfs"
 
+export const SmurfVillage = props => {
+    useEffect(() => {
+        props.fetchSmurfs();
+    }, [])
+    if (props.fetchSmurfs) {
+        return <h2>Loading Smurf Village...</h2>
+    }
+}
