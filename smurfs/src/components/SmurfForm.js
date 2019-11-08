b5e6924f88416b6d857fd651387b671f45d846af
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-08 13:55:55 -0500

    finally fetching data

---
+++ smurfs/src/components/SmurfForm.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Smurf = props => {
+  console.log(props);
+  return (
+    <div>
+      <h1>{props.smurf.name}</h1>
+      <p>Age: {props.smurf.age}</p>
+      <p>Height: {props.smurf.height}</p>
+    </div>
+  );
+};
+
+export default Smurf;
--- smurfs/src/components/SmurfVillage.js
+++ smurfs/src/components/SmurfVillage.js
@@ -1,15 +1,40 @@
 import React, { useEffect } from "react";
-import { connect } from "react-redux"; 
+import { connect } from "react-redux";
 
 import { fetchSmurfs } from "../actions/actions";
 import { addSmurf } from "../actions/actions";
-import Smurfs from "./Smurfs"
 
-export const SmurfVillage = props => {
-    useEffect(() => {
-        props.fetchSmurfs();
-    }, [])
-    if (props.fetchSmurfs) {
-        return <h2>Loading Smurf Village...</h2>
-    }
+const SmurfVillage = props => {
+  useEffect(() => {
+    props.fetchSmurfs();
+  }, []);
+  if (props.fetchingSmurfs) {
+    return <h2>Loading Smurf Village...</h2>;
+  }
+  return (
+    <div>
+      {props.state && <p>{props.error}</p>}
+      {props.smurfs.map(smurf => (
+        <div>
+          <h1>Name: {smurf.name}</h1>
+          <p>Age: {smurf.age}</p>
+          <p>Height: {smurf.height}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = state => {
+  console.log("current state", state);
+  return {
+    smurfs: state.smurfs,
+    fetchingSmurfs: state.fetchingSmurfs,
+    error: state.error
+  };
 }
+
+export default connect(
+  mapStateToProps,
+  { fetchSmurfs, addSmurf }
+)(SmurfVillage);
