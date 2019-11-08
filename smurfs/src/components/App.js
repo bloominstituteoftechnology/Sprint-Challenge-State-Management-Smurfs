4cd6d6491bd97fb3b3a8c5416916abffc788b8ac
Nicole Adams <nicoleadams.boston@gmail.com>
2019-11-08 11:28:26 -0500

    Add Provider and store

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from "react";
+import { connect } from "react-redux"
 import "./App.css";
+
+
+
+
 class App extends Component {
   render() {
     return (
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { smurfsReducer } from './smurfsReducer';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const store = createStore(smurfsReducer, applyMiddleware(thunk, logger));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
