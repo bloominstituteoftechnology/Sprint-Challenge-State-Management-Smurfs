447b6b74761669c5b5b8b3956d769e251afbc7a0
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-11-08 12:27:51 -0500

    actions and reducer, added new folders

--- smurfs/src/reducers/reducer.js
+++ smurfs/src/reducers/reducer.js
@@ -1,4 +1,5 @@
 import {
+  ADD_SMURF,
   START_FETCHING,
   FETCH_SUCCESS,
   FETCH_FAILURE
@@ -17,10 +18,33 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
     switch(action.type) {
-        case: 
+        case ADD_SMURF: 
         const newSmurf = {
-            name: 
-        }
+            name: action.payload,
+            age: "",
+            height: "",
+            // id: Date.now()
+        };
+        return {...state, smurfs: [...state.smurfs, newSmurf]};
+
+        case START_FETCHING: 
+        return {
+            ...state,
+            error: ""
+        };
+        case FETCH_SUCCESS:
+            return {
+                ...state,
+                error: "",
+                smurfs: action.payload
+            };
+
+            case FETCH_FAILURE:
+                return {
+                    ...state,
+                    error: action.payload,
+                };
+                default: return state;
     }
 };
 
