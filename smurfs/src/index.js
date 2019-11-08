ce5207b27d520f33c38b7e904a3cda6c0afb5211
Darrin Lowery <lambda@monsto.com>
2019-11-08 10:27:58 -0600

    outer index setup

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { applyMiddleware, createStore } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
 import "./index.css";
 import App from "./components/App";
+import reducer from "./reducers"
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(reducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById("root")
+);
