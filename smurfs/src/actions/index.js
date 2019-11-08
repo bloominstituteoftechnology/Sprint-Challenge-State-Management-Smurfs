07d0f6f8b03e67dfa2c5c9feecff77ee3f6d0c02
Darrin Lowery <lambda@monsto.com>
2019-11-08 12:05:05 -0600

    Add smurf works

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -1,15 +1,21 @@
 import axios from "axios";
 
-export const SMURF_LOAD = "BB_LOAD";
-export const SMURF_SUCCESS = "BB_SUCCESS";
-export const SMURF_FAILED = "BB_FAILED";
+export const SMURF_LOAD = "SMURF_LOAD";
+export const SMURF_SUCCESS = "SMURF_SUCCESS";
+export const SMURF_FAILED = "SMURF_FAILED";
+
+export const WRITE_TRY = "WRITE_LOAD";
+export const WRITE_SUCCESS = "WRITE_SUCCESS";
+export const WRITE_FAILED = "WRITE_FAILED";
 
 function clg(...x) {
 	for (let exes of x) console.log(exes);
 }
 
+// API url
 const smurfurl = "http://localhost:3333/smurfs";
 
+// GET actions
 export const smurfLoad = () => ({ type: SMURF_LOAD });
 
 export const smurfLoadSucc = data => ({
@@ -21,12 +27,46 @@ export const smurfLoadFailure = err => ({
 	payload: err
 });
 
+// POST actions
+export const writeTry = () => ({ type: WRITE_TRY });
+
+export const writeSucc = data => ({
+	type: WRITE_SUCCESS,
+	payload: data
+});
+export const writeFailure = err => ({
+	type: WRITE_FAILED,
+	payload: err
+});
+
 export function smurfGet() {
 
 	return function(dispatch) {
 		
 		dispatch(smurfLoad());
-		return axios(smurfurl)
+		return axios.get(smurfurl)
+			.then(res => {
+				clg("LOADING");
+				clg(res.data);
+				return res.data;
+			})
+			.then(json => {
+				clg("SUCCESS");
+				return dispatch(smurfLoadSucc(json));
+			})
+			.catch(err => {
+				clg("Failed. Problem.");
+				return dispatch(smurfLoadFailure(err));
+			});
+	};
+}
+
+export function smurfPost(send) {
+
+	return function(dispatch) {
+		
+		dispatch(writeTry());
+		return axios.post(smurfurl, send)
 			.then(res => {
 				clg("LOADING");
 				clg(res.data);
--- smurfs/src/components/Smurfy.js
+++ smurfs/src/components/Smurfy.js
@@ -1,21 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { connect } from "react-redux";
+import { smurfGet, smurfPost } from "../actions/index";
 
 function clg(...x) {
 	for (let exes of x) console.log(exes);
 }
 
-const Smurfy = (props) => {
+const Smurfy = props => {
+	const [name, setName] = useState("");
+	const [age, setAge] = useState("");
+	const [height, setHeight] = useState("");
+
+	const handleName = e => {
+		clg("handleName", e.target.value);
+		setName(e.target.value);
+	};
+
+	const handleAge = e => {
+		clg("handleAge", e.target.value);
+		setAge(e.target.value);
+	};
+
+	const handleHeight = e => {
+		clg("handleHeight", e.target.value);
+		setHeight(e.target.value);
+	};
+
+	const handleSubmit = e => {
+		clg("handleSubmit", e.target.value);
+
+		e.preventDefault();
+		props.smurfPost({ name: name, age: age, height: height });
+		setName("");
+		setAge("");
+		setHeight("");
+	};
 
 	return (
 		<>
 			{props.isFetching && <div>Loading . . .</div>}
 			{props.error && <div>{props.error.message}</div>}
-			<ul>
-				{props.smurfList.map(e => (
-					<li key={e.name}>{e.name}</li>
-				))}
-			</ul>
+
+			{/* `if` smurflist != null, then render the form and smurflist */}
+			{props.smurfList.length != null && (
+				<>
+					<form>
+						<input type="text" onChange={handleName} name="name" placeholder="New Smurf Name" />
+						<input type="text" onChange={handleAge} name="age" placeholder="Age" />
+						<input type="text" onChange={handleHeight} name="height" placeholder="Height" />
+						<button type="submit" onClick={handleSubmit}>
+							Add Smurf!
+						</button>
+					</form>
+					<ul>
+						{props.smurfList.map(e => (
+							<li key={e.name}>{e.name}</li>
+						))}
+					</ul>
+				</>
+			)}
 		</>
 	);
 };
@@ -23,5 +66,6 @@ const Smurfy = (props) => {
 const mapStateToProps = state => state;
 
 export default connect(
-	mapStateToProps, {}
+	mapStateToProps,
+	{ smurfPost, smurfGet }
 )(Smurfy);
