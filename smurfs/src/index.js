7b33487a6359f22afc58db6827495351e875a9b6
HarrisonMS <54726103+HarrisonMS@users.noreply.github.com>
2019-11-09 10:48:17 -0800

    built out some structure added store and a reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import {smurfReducer} from "./reducers/smurfReducer"
+
+const store = createStore(smurfReducer, applyMiddleware(thunk));
+
+
+ReactDOM.render(<Provider><App /></Provider>, document.getElementById("root"));
