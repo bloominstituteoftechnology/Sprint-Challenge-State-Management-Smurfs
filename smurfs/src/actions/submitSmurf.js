dc9b45688b2f26fd87eae89f1252eee028a875c7
Lambda_School_Loaner_181 <lambda_school_loaner_181@Lambda-School-Loaner-181s-MacBook-Pro.local>
2019-11-22 21:00:19 -0600

    finished

---
+++ smurfs/src/actions/submitSmurf.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+export const SUBMIT_SMURF = 'SUBMIT_SMURF';
+
+export function submitSmurf(formData) {
+    return dispatch => {
+        dispatch({type: SUBMIT_SMURF, payload: formData})
+        axios
+            .post('http://localhost:3333/smurfs', formData)
+                .then((res) => {
+                    console.log(res);
+                })
+    }
+}
