45cd44e667164adcc0c41e3bd4a36797aa8a7472
osogrizz <lhtor@gmail.com>
2019-11-24 18:20:54 -0800

    Adds store. Creates action and reducer directories.

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+// import thunk from 'redux-thiunk';
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root"));
