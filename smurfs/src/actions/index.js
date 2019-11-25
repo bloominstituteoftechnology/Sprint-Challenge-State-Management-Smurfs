198919c94331691d288970b53a80542849ee13d3
osogrizz <lhtor@gmail.com>
2019-11-24 18:53:02 -0800

    Adds all actions.

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,6 +4,11 @@ export const GET_SMURFS_START = 'GET_SMURFS_START';
 export const GET_SMURFS_SUCCESS = 'GET_SMURFS_SUCCESS';
 export const GET_SMURFS_FAIL = 'GET_SMURFS_FAIL';
 
+export const ADD_SMURF_START = 'ADD_SMURF_START';
+export const ADD_SMURF_SUCCESS = 'ADD_SMURF_SUCCESS';
+export const ADD_SMURF_FAIL = 'ADD_SMURF_FAIL';
+
+
 
 export const getSmurfs = () => dispatch => {
   dispatch({  type: GET_SMURFS_START });
@@ -22,6 +27,21 @@ export const getSmurfs = () => dispatch => {
     dispatch({
       type: GET_SMURFS_FAIL,
       payload: 'No smurfs available.'
+    })
+  })
+}
+
+export const addSmurf = () => dispatch => {
+  dispatch({ type: ADD_SMURF_START });
+
+  axios
+  .post('')
+  .then()
+  .catch(err => {
+    console.log(err)
+    dispatch({
+      type: ADD_SMURF_FAIL,
+      payload: 'Failed to add smurf!'
     })
   })
 }
--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,4 +1,4 @@
-import { GET_SMURFS_START, GET_SMURFS_SUCCESS, GET_SMURFS_FAIL } from "../actions";
+import { GET_SMURFS_START, GET_SMURFS_SUCCESS, GET_SMURFS_FAIL,ADD_SMURF_START, ADD_SMURF_SUCCESS, ADD_SMURF_FAIL } from "../actions";
 
 
 const initialState = {
@@ -21,7 +21,22 @@ const reducer = (state = initialState, action) => {
         ...state,
         smurfs: [...state.smurfs, ...action.payload]
       }
+      
     case GET_SMURFS_FAIL:
+      return {
+        error: action.payload
+      }
+      
+    case ADD_SMURF_START:
+      return {
+        ...state
+      }
+
+    case ADD_SMURF_SUCCESS:
+      return {
+        ...state
+      }
+    case ADD_SMURF_FAIL:
       return {
         error: action.payload
       }
