9ebf659dc147372371af8e999ee4f3ebf33143ad
Veto Ramirez <vetorobert3@gmail.com>
2019-12-02 20:26:02 -0800

    created index file in the new actions folder to bring in data from localhost:3333/smurf

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import axios from 'axios';
+
+export const SMURF_DATA_START = 'SMURF_DATA_START';
+export const SMURF_DATA_START = 'SMURF_DATA_SUCCESS';
+
+export const getSmurfData = () => dispatch => {
+  dispatch ({ type: SMURF_DATA_START });
+
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res =>
+      dispatch({
+        type: SMURF_DATA_SUCCESS,
+        payload: { name: res.data.value }
+      })  
+    )
+    .catch(err => console.log(err));
+};
