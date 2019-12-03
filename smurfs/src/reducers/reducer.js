76d61678f22c6730d15bff70f80af13c099283b5
Veto Ramirez <vetorobert3@gmail.com>
2019-12-02 20:59:54 -0800

    did more work in the reducer

--- smurfs/src/reducers/reducer.js
+++ smurfs/src/reducers/reducer.js
@@ -1,4 +1,23 @@
-function reducer() {
+import {
+  SMURF_DATA_START,
+  SMURF_DATA_SUCCESS
+} from '../actions';
+
+const reducer = (state, action) => {
+  switch(action.type) {
+    case SMURF_DATA_START:
+      return {
+        ...state,
+        isLoading: true
+      };
+    case SMURF_DATA_SUCCESS:
+      return {
+        ...state,
+        isLoading: false
+      };
+    default:
+      return state;
+  }
   return {
     name: '',
     age: '',
