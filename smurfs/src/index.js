b4f3f94ce5ccfd8156a3b92eb78f129431df205f
benjamin Soltgit config user.email bensolt@gmail.com <bensolt@gmail.com>
2019-12-13 11:45:55 -0500

    added middlware to src index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//import CreateStore and Provider
+import {createStore, applyMiddleware, compose} from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancers = compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
+
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>, document.getElementById("root"));
