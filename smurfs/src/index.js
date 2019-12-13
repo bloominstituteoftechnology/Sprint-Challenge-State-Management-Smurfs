c9b9d9bf319e6d42d52f333a5fdaf46410cecc04
Hunter5555 <hunter.m.allan@gmail.com>
2019-12-13 11:46:55 -0700

    mvp

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
