544897e2a9eebd2596fe2378cd53f8d484260c2d
jarrodskahill <jarrodskahill@gmail.com>
2019-12-13 09:33:16 -0800

    created add and remove functions, and added props to provider

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -15,14 +15,27 @@ const App = () => {
       })
   }, []);
 
+  const Add = smurf => {
+		axios.post('http://localhost:3333/smurfs', smurf).then(res => {})
+	};
+
+	const Remove = smurf => {
+		axios
+			.delete(`http://localhost:3333/smurfs/${smurf}`)
+			.then(res => {
+				setSmurfs(res.data);
+			})
+		
+	};
+
     return (
-      <SmurfContext.Provider>
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
+      <SmurfContext.Provider value={{smurfs, setSmurfs, Add, Remove}}>
+        <div className="App">
+          <h1>SMURFS! 2.0 W/ Redux</h1>
+          <div>Welcome to your state management version of Smurfs!</div>
+          <div>Start inside of your `src/index.js` file!</div>
+          <div>Have fun!</div>
+        </div>
       </SmurfContext.Provider>
     );
 
