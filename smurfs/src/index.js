a15d4f489c80763f646d839323fa0ad3800f74a2
DeniseFafette <denisefafette@gmail.com>
2019-12-13 08:49:38 -0800

    imported BrowserRouter and withRouter and wrapped the App with Router

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+<Router>
+    <AppWithRouter />
+    </Router>,
+    document.getElementById('root')
+    );
