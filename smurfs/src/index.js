3f5660414a735fccbc19adaf348e3003bcb9df52
brendantfinn <brendantfinn@gmail.com>
2020-01-10 18:42:39 -0600

    installed redux-thunk & axios / set up index.js

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 import { createStore, applyMiddleware } from 'redux';
-import { Proivder } from 'react-redux';
-import thunk from 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import { reducer } from "./reducers";
+
+const store = createStore(
+    reducer,
+    applyMiddleware(thunk)
+)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,11 @@
+const intialState = {
+    
+}
+
+
+export const reducer = (state = intialState, action) => {
+    switch(action.type){
+        default:
+            return state;
+    }
+}
