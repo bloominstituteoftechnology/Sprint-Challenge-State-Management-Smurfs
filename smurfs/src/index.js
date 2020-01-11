3dbab303500c89b3537484ea87edc60f3f816fa5
brendantfinn <brendantfinn@gmail.com>
2020-01-10 18:19:53 -0600

    inital setup / installed yarn / added dependencies (react, react-redux, redux-thunk) / set up file structure

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
-
+import { createStore, applyMiddleware } from 'redux';
+import { Proivder } from 'react-redux';
+import thunk from 
 ReactDOM.render(<App />, document.getElementById("root"));
