993d1b48c190616bcd055e95be52b89cee1804e5
CLYPR Technologies <cbanks@clypr.co>
2020-01-12 14:22:15 -0500

    completed all components

---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+import Smurf from './Smurf';
+import { connect } from 'react-redux';
+import { fetchSmurfs } from '../actions/actions';
+
+const Smurflist = (props) => {
+    useEffect(() => {
+        props.fetchSmurfs()
+        console.log(props.smurfs)
+    }, [])
+
+    return (
+        <div className='smurflist'>
+            {props.smurfs.map(smurf => {
+                return <Smurf smurf={smurf}/>
+            })}
+        </div>
+    )
+}
+
+const mapStateToProps = (state) => {
+    return {
+        smurfs: state.smurfs
+    }
+}
+
+export default connect(mapStateToProps, {fetchSmurfs})(SmurfList);
