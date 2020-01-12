62b6015458048b0704765bdc79bb3ee809d4cc99
CLYPR Technologies <cbanks@clypr.co>
2020-01-12 14:32:04 -0500

    completed reducers

---
+++ smurfs/src/reducers/reducers.js
@@ -0,0 +1,48 @@
+import {
+    FETCH_SMURFS_START,
+    FETCH_SMURFS_SUCCESS,
+    FETCH_SMURFS_FAILURE,
+    ADD_SMURFS_SUCCESS
+} from '../actions/actions';
+
+const initialState = {
+    smurfs: [],
+    isFetching: false,
+    error: ''
+}
+
+export const reducer = (state = initialState, action) => {
+    switch(action.type) {
+
+        case FETCH_SMURFS_START: 
+            return {
+                ...state,
+                isFetching: true
+            }
+
+        case FETCH_SMURFS_SUCCESS:
+            console.log('yes')
+            return {
+                ...state,
+                smurfs: action.payload.data,
+                isFetching: false,
+                error: ''
+            }
+
+        case FETCH_SMURFS_FAILURE:
+            return {
+                ...state,
+                isFetching: false,
+                error: action.payload.error
+            }
+
+        case ADD_SMURFS_SUCCESS:
+            return {
+                ...state,
+                smurfs: action.payload.data
+            }
+
+        default:
+            return state
+    }
+}
