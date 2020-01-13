fbd7a8f26e26cfcff383fa3a42f212fd11143331
Heather Nye <heatherdnye@gmail.com>
2020-01-12 22:09:24 -0600

    reducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { createStore } from 'redux';
+import { Provier } from 'react-redux';
+import { smurfReducer } from './reducers/index';
+
+import App from "./App";
+
+const store = createStore(smurfReducer);
+console.log(store);
+
+
+
+
+
+ReactDOM.render(
+<Provider store={store}>
+    <App /></Provider>,
+    document.getElementById("root"));
---
+++ smurfs/src/reducers/index.js
@@ -0,0 +1,10 @@
+const intialState = {
+    name: '',
+    age: 200,
+    height: '5cm',
+    id: new Date()
+}
+
+export const smurfReducer = (state = initialState, action) => {
+    return initialState
+}
