b6b659614f45001dfcafae088f3f74e59048e78a
Robert Stewart <rstewart414@gmail.com>
2020-01-13 09:34:26 -0500

    mvp met

---
+++ smurfs/src/components/Smurf.js
@@ -0,0 +1,12 @@
+import React from "react";
+const Smurf = props => {
+  return (
+    <div className="smurf">
+      <h3>{props.smurf.name}</h3>
+      <p>Age: {props.smurf.age}</p>
+      <p>Height: {props.smurf.height}</p>
+    </div>
+  );
+};
+
+export default Smurf;
