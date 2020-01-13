29c843e38a1b9e67157826b20731f28fe1b7c2ae
Your name <a.m.verdine@gmail.com>
2020-01-12 21:46:24 -0500

    Setup store and wrap App in Provider

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+let store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
