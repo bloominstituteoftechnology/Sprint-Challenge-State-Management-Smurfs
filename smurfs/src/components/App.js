f340977b187f19f978845dc3a4fd8174db7873c5
josegocampo <jose.ocampo@mail.udp.cl>
2020-01-13 21:56:24 -0300

    some styling changes

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { connect } from "react-redux";
 import axios from "axios";
 import Form from './Form';
+import S from 'styled-components';
 
 
 import { getSmurfs, deleteSmurfs} from '../actions';
@@ -34,11 +35,13 @@ const App = (props) => {
 
       <div className="App">
 
+        <HMayor>!!!SMURFS!!</HMayor>
+
         {smurfs.map((each) => 
-        <div>
-        <h3>name: {each.name}  age: {each.age}  height: {each.height}
-         <button onClick={()=>{deleteSmurf(each.id)}} >Delete Smurf</button></h3>
-        </div>
+        <Card>
+        <H1>{each.name} </H1> <H2>age: {each.age}</H2>  <H2>height: {each.height}</H2>
+         <Button onClick={()=>{deleteSmurf(each.id)}} > Delete Smurf</Button>
+        </Card>
         )}
 
         <Form/>
@@ -59,3 +62,52 @@ const App = (props) => {
   }, 
       { getSmurfs : getSmurfs, deleteSmurfs : deleteSmurfs } 
   )(App);
+
+
+
+  const Card = S.div`
+     width: 800px;
+     height: 50px;
+     border-radius: 5px;
+     margin: 0 auto;
+     margin-top: 30px;
+     margin-bottom: 30px;
+     background: lightgrey;
+     display: flex;
+     align-items: center;
+     justify-content: space-around;
+     padding: 10px;
+`
+
+
+  const Button = S.button`
+
+  border: 1px solid red;
+  background: orange;
+  width: 100px;
+  height: 25px;
+  border-radius: 5px;
+  margin-left: 10px;
+  font-weight: 600;
+`
+
+
+const HMayor = S.div`
+font-size: 3rem;
+font-weight: 700;
+padding-top: 30px;
+
+`
+
+const H1 = S.div`
+font-size: 1.5rem;
+font-weight: 600;
+
+`
+
+
+const H2 = S.div`
+font-size: 1rem;
+font-weight: 500;
+
+`
--- smurfs/src/components/Form.js
+++ smurfs/src/components/Form.js
@@ -59,9 +59,9 @@ const Form = (props) => {
    return (
     <div className="smurf-form">
         <form onSubmit={handleSubmit} name="smurf-form">
-            <input onChange={handleChange} value={datos.name} name="name" placeholder="name" type="text"/>
-            <input onChange={handleChange} value={datos.age} name="age" placeholder="age" type="text"/>
-            <input onChange={handleChange} value={datos.height} name="height" placeholder="height" type="text"/>
+            <input onChange={handleChange} value={datos.name} name="name" placeholder="name" type="text"/> 
+            <input onChange={handleChange} value={datos.age} name="age" placeholder="age" type="text"/> 
+            <input onChange={handleChange} value={datos.height} name="height" placeholder="height" type="text"/> 
             <button>Add</button>
         </form>
     </div>
