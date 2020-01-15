837483aa7d3f13277e2dcfd30708b7c108b046cf
Your name <a.m.verdine@gmail.com>
2020-01-15 00:04:07 -0500

    Setup initialState and appReducer

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 
+import "./index.css";
+import App from "./components/App";
+
+
+function 
 let store = createStore(reducer);
 
 ReactDOM.render(
