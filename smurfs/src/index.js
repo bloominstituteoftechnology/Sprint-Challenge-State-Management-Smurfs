362455b38fb26b9f828118ff7681dd0d5035159c
Jessica Wolff <jleahwolff@gmail.com>
2020-01-24 11:02:39 -0600

    index.js is good to go

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import rootreducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// First, create the store
+const store = createStore(rootreducer, applyMiddleware(thunk, logger))
+
+
+ReactDOM.render(
+<Provider store={store}>
+    <App />
+</Provider>
+, document.getElementById("root"));
