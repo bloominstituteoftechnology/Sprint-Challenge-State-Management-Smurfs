a8505c38de73d4c0356ede6313847ecc7117606f
kika <anamonteiro430@gmail.com>
2020-01-24 16:12:57 +0000

    boilerplate done

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,19 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleWare } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import { reducer } from './reducers';
+import './index.css';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+//create store
+const store = createStore(reducer, applyMiddleWare(thunk, logger));
+
+ReactDOM.render(
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.getElementById('root')
+);
