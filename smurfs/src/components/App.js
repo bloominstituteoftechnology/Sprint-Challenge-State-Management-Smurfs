063464a339942d2e1e27543ef746ec6d6e5d6b4a
wesley.moody <wgm3190@gmail.com>
2020-01-24 11:39:40 -0600

    App.js built out but nothing's rendering, just a spinning react logo

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,18 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-import { connect } from 'react-redux';
-import axios from 'axios';
+import { connect } from "react-redux";
+import axios from "axios";
 
-import 
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
-}
+import { getSmurfs } from "../actions";
 
-export default App;
+const App = props => {
+  useEffect(() => {
+    const showSmurfs = async () => {
+      axios
+        .get("http://localhost:3333/smurfs")
+        .then(response => {
+          console.log(response.data[0].name);
+          props.getSmurfs(response.data);
+        })
+        .catch(error => console.log(error));
+    };
+    showSmurfs();
+  }, []);
+
+  const bla = props.smurfs;
+
+  return (
+    <div className='App'>
+      {bla.map(e => (
+        <div>
+          <h3>
+            name: {e.name} age: {e.age} height: {e.height}
+          </h3>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default connect(
+  state => {
+    return { smurfs: state.smurfs };
+  },
+  { getSmurfs: getSmurfs }
+)(App);
