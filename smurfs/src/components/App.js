0a4da6d6c7c0167f0b64e6f0be1e46ccf73cf794
qb1968 <you@example.com>
2020-01-24 12:40:05 -0500

    MVP

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
+
+import SmurfList from "./SmurfList";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <h1>WELCOME TO THE SMURFS!</h1>
+        <SmurfList />
       </div>
     );
   }
---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from "react";
+import { connect } from "react-redux";
+
+import { getSmurfs, addSmurf } from "../actions/action";
+import SmurfForm from "./SmurfForm";
+
+import "../components/App.css";
+
+const SmurfList = props =>{
+    const initSmurfs = props.getSmurfs
+    useEffect(() =>{
+        initSmurfs()
+    }, [initSmurfs])
+
+    return (
+        <div className = "smurfs">
+            <div className = "smurfList">
+                {props.smurf.map(smurf =>(
+                    <div className ="eachSmurf" key={smurf.id}>
+                        <p>Name: {smurf.name}</p>
+                        <p>Age: {smurf.age}</p> 
+                        <p>Height: {smurf.height}</p>
+                    </div>
+                ))}
+            </div>
+            <SmurfForm addSmurf = {props.addSmurf}/>
+        </div>
+    );
+};
+
+const mapStateToProps = state =>{
+    return({
+        smurf: state.smurf,
+        error:state.error
+    })
+};
+
+export default connect (mapStateToProps,{getSmurfs, addSmurf})(SmurfList);
