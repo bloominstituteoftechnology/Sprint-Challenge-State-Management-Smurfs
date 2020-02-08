0ab37caf9c027c27bcd8ff75835080d535ade064
Christine Smith <christinesm@google.com>
2020-02-08 09:22:13 -0800

    initial project setup

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { SmurfContext } from "./contexts/SmurfContext";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+
+ReactDOM.render(<SmurfContext.Provider><App /></SmurfContext.Provider>, document.getElementById("root"));
