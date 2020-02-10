a9634294d3b027e57dd1029f1c38973e7ec2666b
alexis-anderson <aanderson9313@gmail.com>
2020-02-10 00:02:31 -0500

    added some stretch

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -5,8 +5,10 @@ export const FETCHING_SMURFS_SUCCESSFUL = "FETCHING_SMURFS_SUCCESSFUL";
 export const FETCHING_SMURFS_FAILED = "FETCHING_SMURFS_FAILED";
 export const ADDING_SMURF_DATA = "ADDING_SMURF_DATA"
 export const ADDING_SMURF_SUCCESSFUL = "ADDING_SMURFS_SUCCESSFUL";
-export const ADDING_SMURF_FAILED =  "ADDING_SMURF_FAILED";
-
+export const ADDING_SMURF_FAILED =  "ADDING_SMURF_FAILED"; 
+export const DELETE_SMURF_START = 'DELETE_SMURF_START';
+export const DELETE_SMURF_SUCCESSFUL = 'DELETE_SMURF_SUCCESSFUL';
+export const DELETE_SMURF_FAILED = 'DELETE_SMURF_FAILED';
 
 export const fetchSmurf = () => dispatch => {
 
@@ -36,4 +38,20 @@ export const addSmurf = (smurf) => dispatch => {
             })
         })
         .catch( err => console.log(err))
+}
+
+export const deleteSmurf = (id) => dispatch => {
+
+    dispatch({ type: DELETE_SMURF_START })
+        axios
+            .delete(`http://localhost:3333/smurfs/${id}`)
+            .then(res => {
+                console.log(res)
+                dispatch({
+                    type: DELETE_SMURF_SUCCESSFUL,
+                    payload: res
+                })
+            })
+            .catch( err => console.log(err))
+    
 }
--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -1,15 +1,30 @@
 import React from 'react';
 import './App.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { deleteSmurf } from '../actions/index';
 
 const Smurf = (props) => {
 
+    const smurfs = useSelector(state => state.smurfs);
+    const dispatch = useDispatch();
+    
+    const handleDelete = (e) => {
+        e.preventDefault()
+        dispatch(deleteSmurf(e.target.value))
+    }
+
     return (
 
-        <div className = "smurf">
+        <div className = "smurf" key = {props.smurf.id}>
+           
+            
             <h2> {props.smurf.name} </h2>
                 <p>Age: {props.smurf.age} </p>
                 <p>Height: {props.smurf.height}</p>
-        </div>
+            
+            <button className = "delete-button" onClick = {handleDelete} value = {props.smurf.id}>Delete {props.smurf.name} smurf</button>
+
+    </div>
     )
 }
 
