db14d87231ee91e72bf401c07b40ddcf2b4839e4
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-02-21 10:19:32 +0100

    smurfContext

---
+++ smurfs/src/components/Smurflist.js
@@ -0,0 +1,7 @@
+import React, { useContext } from "react";
+import { smurfContext } from "../context/smurfContext";
+
+export default function SmurfList() {
+  const context = useContext(smurfContext);
+  return <div>i am stil here</div>;
+}
