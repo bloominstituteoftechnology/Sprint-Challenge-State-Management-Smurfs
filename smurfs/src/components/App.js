025cef61540328e9eeb6724c75ab91f1b7012b5f
unknown <jrhemann@gmail.com>
2020-02-21 12:20:14 -0600

    MVP + Stretch

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,48 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
+import { SmurfContext } from './SmurfContext'
+import axios from 'axios'
+import SmurfList from './SmurfList'
+import SmurfForm from './SmurfForm'
+import styled from 'styled-components'
+
+const Village = styled.div`
+  width: 400px;
+  margin: auto;
+`
+const App = () => {
+const [smurfs, setSmurfs] = useState([])
+const addSmurf = newSmurf => {
+  setSmurfs([...smurfs, newSmurf]);
+}
+
+// const Remove = smurf => {
+//   axios.delete(`http://localhost:3333/smurfs/${smurf}`).then(res => {
+//     setSmurfs(res.data);
+//   });
+// };
+
+useEffect(() => {
+  axios
+    .get('http://localhost:3333/smurfs')
+    .then(res =>{
+      console.log(res.data)
+      setSmurfs(res.data)
+    })
+}, [])
+
+
     return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
+      <SmurfContext.Provider value={{addSmurf, smurfs}}>
+      <Village>
+      
+      <SmurfList/>
+      <SmurfForm/>
+
+      </Village>
+      </SmurfContext.Provider>
     );
-  }
+  
 }
 
 export default App;
