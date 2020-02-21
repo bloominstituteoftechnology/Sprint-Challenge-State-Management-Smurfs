6fe41f196c02f24744295f707432cc9875ed1490
Rufai-Alex <oluwasinaalexrufai@gmail.com>
2020-02-21 10:59:55 +0100

    smurfsDisplayed

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,20 +1,34 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
+import axios from "axios";
 
 import "./App.css";
 import { smurfContext } from "../context/smurfContext";
 import SmurfList from "./Smurflist";
 
-class App extends Component {
-  render() {
-    return (
-      <div className='App'>
-        <smurfContext.Provider value={""}>
-          {" "}
-          <SmurfList />
-        </smurfContext.Provider>
-      </div>
-    );
+function App() {
+  const [smurfList, setsmurfList] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3333/smurfs")
+      .then(resp => {
+        setsmurfList(resp.data);
+      })
+      .catch(() => {
+        debugger;
+      });
+    // setTodo(todoList);
+  }, []);
+  if (!smurfList) {
+    return <div>'loading'</div>;
   }
+
+  return (
+    <div className='App'>
+      <smurfContext.Provider value={smurfList}>
+        <SmurfList />
+      </smurfContext.Provider>
+    </div>
+  );
 }
 
 export default App;
--- smurfs/src/components/Smurflist.js
+++ smurfs/src/components/Smurflist.js
@@ -3,5 +3,19 @@ import { smurfContext } from "../context/smurfContext";
 
 export default function SmurfList() {
   const context = useContext(smurfContext);
-  return <div>i am stil here</div>;
+  debugger;
+  return (
+    <div>
+      {context.map(smurf => {
+        return (
+          <div key={smurf.id}>
+            Name : {smurf.name} <br />
+            Age: {smurf.age}
+            <br />
+            height: {smurf.height}
+          </div>
+        );
+      })}{" "}
+    </div>
+  );
 }
