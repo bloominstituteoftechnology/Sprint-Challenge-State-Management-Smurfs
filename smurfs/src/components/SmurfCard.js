7bf7d4f5107e8ada7ab87656107c347806317232
Logan Negley <logannegley@gmail.com>
2020-03-06 20:36:50 -0600

    styled cards

--- smurfs/src/components/SmurfCard.js
+++ smurfs/src/components/SmurfCard.js
@@ -1,15 +1,33 @@
 import React, {useContext} from 'react';
+import styled from 'styled-components'
+
+const Card = styled.div`
+border: solid black 2px;
+background-color:#3682BA;
+width:50%;
+margin:20px auto;
+`
+
+const H1 = styled.h1`
+color:white;
+font-size:45px;
+`
+
+const P = styled.p`
+color:white;
+font-size:20px;
+`
 
 
 
 const SmurfCard = (props) =>{
 
     return(
-        <div className='smurf-card'>
-            <h1>{props.smurf.name}</h1>
-            <p>{props.smurf.age}</p>
-            <p>{props.smurf.height}</p>
-        </div>
+        <Card className='smurf-card'>
+            <H1>{props.smurf.name}</H1>
+            <P>Age: {props.smurf.age}</P>
+            <P>Height: {props.smurf.height}</P>
+        </Card>
     );
 };
 
