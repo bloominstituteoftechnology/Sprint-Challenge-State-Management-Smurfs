78422e6c40bb995ef1abfce94ea956556b190ecd
Logan Negley <logannegley@gmail.com>
2020-03-06 18:33:36 -0600

    SmurfContext provider wrapped around app comp

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 
 // Context
+import SmurfContext from '../context.js/SmurfContext';
 
 function App () {
 const [data, setData] = useState([])
@@ -13,7 +14,9 @@ const [data, setData] = useState([])
 
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1> 
+      <SmurfContext.Provider value={data}>
+        <h1>SMURFS! 2.0 W/ Redux</h1>
+      </SmurfContext.Provider> 
       </div>
     );
 }
