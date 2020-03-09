f5538cf996d1e58b32d95260323ff081c77a60ae
Your name <a.m.verdine@gmail.com>
2020-03-08 20:09:10 -0400

    Install axios/Build out DisplaySmurf component/Render FETCH response data

--- smurfs/src/components/DisplySmurf.js
+++ smurfs/src/components/DisplySmurf.js
@@ -1,23 +1,31 @@
 import React from "react";
 
 class DisplaySmurf extends React.Component {
-  //   constructor() {
-  //     super();
-  //     this.state = {
-  //       name: "",
-  //       age: "",
-  //       height: ""
-  //     };
-  //   }
+  constructor(props) {
+    super(props);
+    this.state = {
+      smurfs: []
+    };
+  }
 
   componentWillMount() {
-    console.log(123);
+    fetch("http://localhost:3333/smurfs")
+      .then(res => res.json())
+      .then(data => this.setState({ smurfs: data }));
   }
 
   render() {
+    const smurfInfo = this.state.smurfs.map(smurf => (
+      <div key={smurf.id}>
+        <h2>{smurf.name}</h2>
+        <h3>Age: {smurf.age} yrs. old</h3>
+        <h4>Height: {smurf.height}</h4>
+      </div>
+    ));
     return (
       <div>
         <h1>Smurfs</h1>
+        {smurfInfo}
       </div>
     );
   }
