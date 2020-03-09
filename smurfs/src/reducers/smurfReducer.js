026c3c605f37a4dcef3d459c4610c0588095c643
Your name <a.m.verdine@gmail.com>
2020-03-08 21:56:41 -0400

    Add NEW_SMURF action creator to smurfReducer

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -12,6 +12,11 @@ export default function(state = initialState, action) {
         ...state,
         items: action.payload
       };
+    case NEW_SMURF:
+      return {
+        ...state,
+        item: action.payload
+      };
     default:
       return state;
   }
