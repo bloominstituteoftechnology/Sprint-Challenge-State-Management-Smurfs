a96acef9285dee6eb0483e5e116c3af4ba0f3f08
Benjamin S Ogzewalla <saigeogzewalla@gmail.com>
2020-03-08 21:43:56 -0400

    MVP

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
+import {Reducer} from './reducers/Reducer';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const composeEnhance = window.__REDUX_DEVTOOLS_EXTENTION_COMPOSE__|| compose;
+
+const store = createStore(
+    Reducer,
+    composeEnhance(applyMiddleware(thunk))
+);
+
+ReactDOM.render(
+<Provider store = {store}>
+    <App />
+</Provider>, 
+document.getElementById("root"));
