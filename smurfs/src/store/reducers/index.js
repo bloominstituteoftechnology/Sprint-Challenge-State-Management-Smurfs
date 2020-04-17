657498e92021c53e196f5f1c62bf48525bd4595a
gabeaster <gab.easter@live.com>
2020-04-17 11:40:12 -0400

    setup reducer in src/reducers/index.js; set state as initialState and default return state

---
+++ smurfs/src/store/reducers/index.js
@@ -0,0 +1,14 @@
+
+
+const initialState = {
+    smurfs: [],
+    isLoading: false,
+    error: ''
+};
+
+export const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        default: 
+            return state;
+    }
+}
