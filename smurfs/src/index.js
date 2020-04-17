7527d62d74c27a58cad06a8ba473af5fb1a3473a
Gordon Caister <gordoncaister@gmail.com>
2020-04-17 10:27:53 -0500

    created action names

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -3,4 +3,25 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import logger from "redux-logger";
+import reducer from "./reducers";
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+    reducer,
+    composeEnhancers(applyMiddleware(thunk, logger))
+  );
+
+
+ReactDOM.render(
+<Provider>
+    <App store={store}/>
+</Provider>
+
+
+, document.getElementById("root"));
