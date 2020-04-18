a4fac450297dbe3089fab556fd058eec9fe0b7de
Brownbatz <Lopeztgabriel4@gmail.com>
2020-04-17 21:50:36 -0600

    I have hit MVP for this project

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,67 @@
-import React, { Component } from "react";
+import React, { Component, useState, useReducer, useEffect } from "react";
+import axios from "axios";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+
+// context
+import smurfList from "../contexts/smurflistcontext";
+
+// Component
+import SmurfVillage from "./smurfVillage";
+
+function App() {
+  const [smurfs, setSmurfs] = useState([])
+  const [smurfName, setSmurfName] = useState('');
+  const [smurfAge, setSmurfAge] = useState(0);
+  const [smurfHeight, setSmurfHeight] = useState('');
+
+  useEffect(() => {
+    axios.get('http://localhost:3333/smurfs')
+    .then(result => {
+        setSmurfs(result.data)
+    })
+    .catch(error => {console.log('caught an error: ',error)})
+  }, [])
+
+  return (
+    <div className="App">
+      <smurfList.Provider value={smurfs}>
+        <SmurfVillage />
+        <form onSubmit={(e) => {
+          e.preventDefault()
+          const newSmurf={
+            name: smurfName,
+            age: smurfAge,
+            height: smurfHeight,
+            id: smurfs.length
+          }
+          axios.post("http://localhost:3333/smurfs", newSmurf)
+            .then(r => console.log(r.data))
+
+          }}>
+          <h3>New Smurf Name</h3>
+          <input 
+            type="text"
+            onChange={(e) => setSmurfName(e.target.value)}
+          />
+          <h3>New Smurf Age</h3>
+          <input 
+            type="num"
+            onChange={(e) => setSmurfAge(e.target.value)}
+          />
+          <h3>New Smurf Height</h3>
+          <input
+            type="text"
+            onChange={(e) => {setSmurfHeight(e.target.value)}}
+          />
+          <input
+            type="submit"
+            value="submit form"
+          />
+        </form>
+      </smurfList.Provider>
+    </div>
+  );
+
 }
 
 export default App;
---
+++ smurfs/src/contexts/smurflistcontext.js
@@ -0,0 +1,5 @@
+import {createContext} from "react";
+
+const smurfList = createContext();
+
+export default smurfList;
