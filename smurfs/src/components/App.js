a6c4457ecb1c0fe3b90258d4f1f079bf117048a5
Austin Burton <abur3460@gmail.com>
2020-04-18 11:23:09 -0600

    small code changes

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 import "./App.css";
-import { getSmurfs } from '../actions/getSmurfs';
-import { submitSmurf } from '../actions/submitSmurf';
+import { getSmurfs } from "../actions/getSmurfs";
+import { submitSmurf } from "../actions/submitSmurf";
 
 function App(props) {
-  const [name, setName] = useState('');
-  const [age, setAge] = useState('');
-  const [height, setHeight] = useState('');
+  const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [height, setHeight] = useState("");
 
   useEffect(() => {
     props.getSmurfs();
-  }, [])
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -19,65 +19,76 @@ function App(props) {
       name: name,
       age: age,
       height: height,
-      id: Date.now()
+      id: Date.now(),
     });
-    setName('')
-    setAge('')
-    setHeight('')
-  }
+    setName("");
+    setAge("");
+    setHeight("");
+  };
 
-  
-  const handleNameChange = e => {
+  const handleNameChange = (e) => {
     e.preventDefault();
-    setName(e.target.value)
-  }
+    setName(e.target.value);
+  };
 
-  const handleAgeChange = e => {
+  const handleAgeChange = (e) => {
     e.preventDefault();
-    setAge(e.target.value)
-  }
+    setAge(e.target.value);
+  };
 
-  const handleHeightChange = e => {
+  const handleHeightChange = (e) => {
     e.preventDefault();
-    setHeight(e.target.value)
-  }
+    setHeight(e.target.value);
+  };
   return (
     <div className="App">
-      <h1>SMURFS! 2.0 W/ Redux</h1>
-      <div>Welcome to your state management version of Smurfs!</div>
-      <div>Start inside of your `src/index.js` file!</div>
-      <div>Have fun!</div>
-
+      <h1>Smurf Village:</h1>
       <form onSubmit={handleSubmit}>
-        <input placeholder='Name' name='Name' value={name} onChange={handleNameChange} />
-        <input placeholder='Age' name='Age' value={age} onChange={handleAgeChange} />
-        <input placeholder='Height in cm' name='Height' value={height} onChange={handleHeightChange} />
+        <input
+          placeholder="Name"
+          name="Name"
+          value={name}
+          onChange={handleNameChange}
+        />
+        <input
+          placeholder="Age"
+          name="Age"
+          value={age}
+          onChange={handleAgeChange}
+        />
+        <input
+          placeholder="Height in cm"
+          name="Height"
+          value={height}
+          onChange={handleHeightChange}
+        />
         <button onClick={handleSubmit}>Submit</button>
       </form>
 
-    <div className='smurf-cards'>
+      <div className="smurf-cards">
         {props.smurfs.map((smurf) => {
-          return <div className='smurf-card' key={smurf.id}>
-            <h2>{smurf.name}</h2>
-            <h4>Age: {smurf.age}</h4>
-            <h4>Height: {smurf.height}</h4>
-          </div>
+          return (
+            <div className="smurf-card" key={smurf.id}>
+              <h2>{smurf.name}</h2>
+              <h4>Age: {smurf.age}</h4>
+              <h4>Height: {smurf.height}cm</h4>
+            </div>
+          );
         })}
       </div>
-
     </div>
   );
 }
 
 function mapStateToProps(state) {
   return {
-    smurfs: state.smurfs
-  }
+    smurfs: state.smurfs,
+  };
 }
 
 const mapDispatchToProps = {
   getSmurfs,
-  submitSmurf
-}
+  submitSmurf,
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
