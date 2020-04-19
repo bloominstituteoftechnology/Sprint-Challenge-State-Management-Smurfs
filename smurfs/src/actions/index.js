39869ab04e3cd12656dea02f662aa9bc43c30e72
webbmaxwell <webbwrites@gmail.com>
2020-04-18 22:42:03 -0600

    imported variables into src/index.js. Set up axios 'get' in actions/index.js.

---
+++ smurfs/src/actions/index.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export const START_FETCHING = 'START_FETCHING';
+export const FETCH_SUCCESS = 'FETCH_SUCCESS';
+export const FETCH_FAILURE = 'FETCH_FAILURE';
+
+export const fetchSmurfs = () => (dispatch) => {
+  dispatch({type: START_FETCHING });
+  axios
+    .get('http://localhost:3333')
+    .then((res) =>
+      dispatch({
+        type: FETCH_SUCCESS,
+        payload: res.data
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: FETCH_FAILURE,
+        payload: err.response
+      })
+    )
+};
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
