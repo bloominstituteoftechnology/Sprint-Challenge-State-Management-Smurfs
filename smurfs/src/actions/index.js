839fd6b5daad86c0dcd0d051f58bb5ca2b8393a3
webbmaxwell <webbwrites@gmail.com>
2020-04-18 23:48:14 -0600

    Started working on post request in actions/index.js

--- smurfs/src/actions/index.js
+++ smurfs/src/actions/index.js
@@ -4,6 +4,10 @@ export const START_FETCHING = 'START_FETCHING';
 export const FETCH_SUCCESS = 'FETCH_SUCCESS';
 export const FETCH_FAILURE = 'FETCH_FAILURE';
 
+export const START_ADDING = 'START_ADDING';
+export const SMURF_ADDED = 'SMURF_ADDED';
+export const ADD_FAILED = 'ADD_FAILED';
+
 export const fetchSmurfs = () => (dispatch) => {
   dispatch({type: START_FETCHING });
   axios
@@ -20,4 +24,22 @@ export const fetchSmurfs = () => (dispatch) => {
         payload: err.response
       })
     )
+};
+
+export const addSmurf = (smurf) => (dispatch) => {
+  dispatch({ type: START_ADDING })
+  axios
+    .get('http://localhost:3333/smurfs', smurf)
+    .then((res) => {
+      dispatch({
+        type: SMURF_ADDED,
+        payload: res.data,
+      })
+    })
+    .catch((err) => {
+      dispatch({
+        type: ADD_FAILED,
+        payload: err.response
+      })
+    })
 };
