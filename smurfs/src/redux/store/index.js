179d2b398b4c2a7f844a576f604db082604cf2c3
victor tran <victor.tran@tempestas.io>
2020-04-19 23:00:14 -0400

    update store to work with browser extension

--- smurfs/src/redux/store/index.js
+++ smurfs/src/redux/store/index.js
@@ -1,8 +1,11 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { rootReducer } from '../reducers';
-import { applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(
+    rootReducer,
+    composeEnhancers(applyMiddleware(thunk)),
+);
 
 export default store;
