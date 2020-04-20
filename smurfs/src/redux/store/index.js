209d67bf70597b95b2734db99bda2c188c45557c
victor tran <victor.tran@tempestas.io>
2020-04-19 21:36:36 -0400

    apply thunk middleware

--- smurfs/src/redux/store/index.js
+++ smurfs/src/redux/store/index.js
@@ -1,6 +1,8 @@
 import { createStore } from 'redux';
 import { rootReducer } from '../reducers';
+import { applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 export default store;
