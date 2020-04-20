bb46b4fb0f9ddbac29374818f86bd7e7c50dd84d
victor tran <victor.tran@tempestas.io>
2020-04-19 21:14:10 -0400

    set up initial reducer and export

---
+++ smurfs/src/redux/reducers/index.js
@@ -0,0 +1,10 @@
+const initialState = {};
+
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+};
+
+export { rootReducer };
