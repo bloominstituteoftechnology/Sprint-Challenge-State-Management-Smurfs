50e9e08278e93b06bedfd2ed5e19b0e3e32a5946
victor tran <victor.tran@tempestas.io>
2020-04-19 21:43:47 -0400

    add initial state and some cases in switch statement for getting smurfs

--- smurfs/src/redux/reducers/index.js
+++ smurfs/src/redux/reducers/index.js
@@ -1,7 +1,44 @@
-const initialState = {};
+import {
+    GET_SMURFS_START,
+    GET_SMURFS_FINISH,
+    GET_SMURFS_FAIL,
+} from '../actions';
+
+const initialState = {
+    smurfs: [],
+    fetchingSmurfs: false,
+    fetchingSmurfsError: {
+        status: null,
+        message: null,
+    },
+};
 
 const rootReducer = (state = initialState, action) => {
     switch (action.type) {
+        case GET_SMURFS_START:
+            return {
+                ...state,
+                fetchingSmurfs: true,
+            };
+
+        case GET_SMURFS_FINISH:
+            return {
+                ...state,
+                smurfs: action.payload,
+                fetchingSmurfs: false,
+            };
+
+        case GET_SMURFS_FAIL:
+            return {
+                ...state,
+                fetchingSmurfs: false,
+                fetchingSmurfsError: {
+                    ...state.fetchingSmurfsError,
+                    status: action.payload.status,
+                    message: action.payload.statusText,
+                },
+            };
+
         default:
             return state;
     }
