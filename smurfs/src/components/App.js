81e3bbba9ebde27dda4d920b00862db6c25c5291
victor tran <victor.tran@tempestas.io>
2020-04-19 21:35:39 -0400

    change App to functional component instead of class and add getSmurfAsync action creator and use it

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,25 @@
-import React, { Component } from "react";
-import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
-}
+import React, { useEffect } from 'react';
+import './App.css';
 
-export default App;
+import { connect } from 'react-redux';
+import { getSmurfAsync } from '../redux/actionCreators';
+
+const App = ({ getSmurfAsync }) => {
+    useEffect(() => {
+        getSmurfAsync();
+    }, []);
+    return (
+        <div className="App">
+            <h1>SMURFS! 2.0 W/ Redux</h1>
+            <div>Welcome to your state management version of Smurfs!</div>
+            <div>Start inside of your `src/index.js` file!</div>
+            <div>Have fun!</div>
+        </div>
+    );
+};
+
+const mapStateToProps = (state) => {
+    return {};
+};
+
+export default connect(mapStateToProps, { getSmurfAsync })(App);
