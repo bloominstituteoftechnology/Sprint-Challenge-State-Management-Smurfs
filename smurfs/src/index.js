a43f29bc6a0ff05107906ec3fd7458d6aac60c3e
victor tran <victor.tran@tempestas.io>
2020-04-19 21:16:54 -0400

    hook up redux in application

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/App';
+import { Provider } from 'react-redux';
+import { store } from './redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
