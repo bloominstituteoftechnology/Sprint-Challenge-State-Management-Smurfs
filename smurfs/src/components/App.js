bff6e15bc963c562ab72a661dc5a9033a89a3fe5
victor tran <victor.tran@tempestas.io>
2020-04-19 22:59:35 -0400

    add switch, routes, and links

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react';
+import { Switch, Route, Redirect, Link } from 'react-router-dom';
 import './App.css';
 
 import { connect } from 'react-redux';
@@ -16,8 +17,34 @@ const App = ({
     }, []);
     return (
         <div className="App">
-            {fetchingSmurfs && <p>Fetching ... </p>}
-            <Form />{' '}
+            <nav>
+                <Link to="/">home</Link>
+                <Link to="/add">add</Link>
+            </nav>
+            <Switch>
+                <Route path="/add" component={Form} />
+                <Route
+                    path="/"
+                    exact
+                    render={(props) => {
+                        return (
+                            <>
+                                {fetchingSmurfs && <p>Fetching ... </p>}
+
+                                {!fetchingSmurfs &&
+                                    smurfs.map(({ id, name, age, height }) => (
+                                        <div key={id}>
+                                            <p>Say hi to {name}.</p>
+                                            <p>They are {age} years old.</p>
+                                            <p>And this tall: {height}.</p>
+                                        </div>
+                                    ))}
+                            </>
+                        );
+                    }}
+                />
+                <Redirect to="/" />
+            </Switch>
         </div>
     );
 };
