5e04b8d336758f81a91e88916ee0d0ebb9fb77ca
Your Name <Reginaldbmiller@gmail.com>
2020-05-01 20:10:01 -0400

    initial push

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,11 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(applyMiddleware(thunk))
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
