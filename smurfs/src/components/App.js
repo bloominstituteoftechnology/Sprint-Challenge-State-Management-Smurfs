3d8d82896efe835239a7b7826e6b22e53dba0d41
Kim Ly Buck <kim.3993@gmail.com>
2020-05-03 21:01:33 -0500

    add smurfs component

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,21 @@
-import React, { Component } from "react";
-import "./App.css";
+import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import './App.css';
+import store from '../store';
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Provider store={stora}>
+				<div className="App">
+					<h1>SMURFS! 2.0 W/ Redux</h1>
+					<div>Welcome to your state management version of Smurfs!</div>
+					<div>Start inside of your `src/index.js` file!</div>
+					<div>Have fun!</div>
+				</div>
+			</Provider>
+		);
+	}
 }
 
 export default App;
---
+++ smurfs/src/components/Smurfs.js
@@ -0,0 +1,3 @@
+export default function Smurfs (){
+    return <div/>
+}
