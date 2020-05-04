927a54abf6cbdd8d7cf2a78ef63ac984bb689e0a
wtrawlings <rawlings.warren@gmail.com>
2020-05-04 19:29:46 -0400

    have data in form

--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,14 +1,26 @@
 import React from 'react'
-
+import { useForm } from 'react-hook-form';
 
 
 
 const SmurfForm = () => {
+
+    const {register, handleSubmit} = useForm();
+    const onSubmit = (data) => {
+        //event.preventDefault();
+        console.log(data);
+    }
+
     return (
-        <h1>
-            This is a Test for the SmurfForm
-        </h1>
+        <div className="SmurfForm">
+            <form onSubmit={handleSubmit(onSubmit)}>
+                <input type="text" placeholder="Smurfy Name" name="name" ref={register} />
+                <input type="text" placeholder="Smurfy Age" name="age" ref={register}/>
+                <input type="text" placeholder="Smurfy Height" name="height" ref={register} />
+                <button type="submit">Add a Smurf to the Villiage</button>
+            </form>
+        </div>
     )
 }
 
-export default SmurfForm
+export default SmurfForm;
