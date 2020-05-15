e168dea0a95bb935b9dac328562137f529fecb13
unknown <hobsond26@icloud.com>
2020-05-15 12:22:05 -0400

    using useContext i created a connection to the database and a function to add new items to the database that will trigger a use state to trigger a change effect

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,76 @@
-import React, { Component } from "react";
+import React, { Component,useContext,useState,useEffect } from "react";
+import axios from 'axios'
+import {DataContext} from '../context/DataContext'
+import {} from 'react-router-dom'
+import { css } from "@emotion/core";
+import ClipLoader from "react-spinners/ClipLoader";
+
+import SmurfList from '../components/SmurfList'
+import SmurfForm from '../components/SmurfForm'
+import Nav from '../components/Nav'
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
+function App(){ 
+  const [data,setData] = useState([]);
+  const [fetching,setFetching] = useState(false)
+  const [newInput ,setNewInput]= useState(0)
+  useEffect(()=>{
+    axios.get('http://localhost:3333/smurfs')
+      .then(res=>{
+        setFetching(true)
+        setData(res.data)
+        console.log(data)
+        setTimeout(()=>{
+          setFetching(false)
+        },2000)
+      
+      })
+  },[newInput])
+
+  const addToDatabase = item=>{
+    axios.post('http://localhost:3333/smurfs',item)
+    .then(()=>{
+      setNewInput(newInput + 1)
+
+      console.log('succsss')
+    })
   }
+  const override = css`
+  display: block;
+  margin: 25% auto 0  auto;
+  border-color: red;
+`;
+  return(
+    <div>
+      <Nav />
+
+      <DataContext.Provider value={{data,setFetching}}>
+        {!fetching ?
+        <div>
+          <SmurfForm />
+
+          <SmurfList />
+
+        </div>
+         
+        :
+        <div className="sweet-loading">
+        <p>Loading ....</p>
+
+        <ClipLoader
+          css={override}
+          size={150}
+          color={"#123abc"}
+          loading={fetching}
+        />
+      </div>
+      
+      }
+
+      </DataContext.Provider>
+
+    </div>
+  )
+
 }
 
 export default App;
---
+++ smurfs/src/components/SmurfList.js
@@ -0,0 +1,11 @@
+import React, {useContext} from 'react'
+import {DataContext} from '../context/DataContext'
+
+export default function SmurfList() {
+    const database = useContext(DataContext)
+    return (
+        <div>
+            
+        </div>
+    )
+}
