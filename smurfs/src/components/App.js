0f8102413bb71dd3090806862e31d5216141ac5b
John Rossi <DACOOK1983@gamil.com>
2020-05-29 21:59:40 -0400

    mvp

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,24 @@
 import React, { Component } from "react";
+import Smurfs from './Smurfs'
+import SmurfsForm from './SmurfsForm'
 import "./App.css";
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <div className="smurf">
+          <h1>SMURFS</h1>
+          <Smurfs />
+        </div>
+        <div className="smurfs-form">
+          <h1>Add New Smurfs To Smurf Village</h1>
+          <SmurfsForm />
+        </div>
       </div>
     );
   }
 }
+
 
 export default App;
--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -2,5 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    rootElement
+);
