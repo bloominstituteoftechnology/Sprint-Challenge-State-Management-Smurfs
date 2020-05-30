95289bd2925788334f1e619fefd15561843afe33
Brian Clayton <finalb@projectfinalboss.com>
2020-05-29 21:36:44 -0400

    initial commit

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App";
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import {applyMiddleware} from 'redux';
+
+ 
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
