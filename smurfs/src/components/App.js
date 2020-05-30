f7ceb19c49283fd5efe2fb9360c7e03fe4ecfe9f
lex-marie790 <lexi.marie790@gmail.com>
2020-05-29 18:28:26 -0700

    App.js - importing files & editing the page

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,13 +1,21 @@
 import React, { Component } from "react";
 import "./App.css";
+import axios from 'axios';
+
+//context
+import { SmurfContext } from '../context/smurfcontext';
+
+
+//components
+import SmurfForm from './smurfform';
+import SmurfList from './smurflist';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
+        <SmurfForm />
+        <SmurfList />
       </div>
     );
   }
