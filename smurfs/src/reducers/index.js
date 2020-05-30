2fcfdbbb48ab7eeb01c32fde7ba16113f3a49e5a
Brian Clayton <finalb@projectfinalboss.com>
2020-05-29 22:11:52 -0400

    add form.js andwork on reducer

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -4,11 +4,11 @@ const initialState ={
 
     isLoading: false,
     smurf: [],
-    add: {
+    add: [{
         name: "",
         age: "",
         height: ""
-    },
+    }],
     error: ""
 }
 
@@ -16,9 +16,28 @@ export const reducer = (state = initialState, action) => {
 
     switch(action.type){
         case START:
+            return {
+                ...state,
+                isLoading: true
+            }
         case SUCCESS:
+            return{
+                ...state,
+                isLoading: false,
+                smurf: action.payload
+            }
         case ADD:
+            return{
+                ...state,
+                isLoading: false,
+                add: [...state.add, action.payload]
+            }
         case FAILED:
+            return{
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }
                 
     }
 }
