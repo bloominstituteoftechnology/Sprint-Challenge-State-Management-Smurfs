20600922b1319dfb75be99505ba6c51845f66330
lex-marie790 <lexi.marie790@gmail.com>
2020-05-29 20:45:40 -0700

    mvp reached- FINALLY!

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, useState, useEffect } from "react";
 import "./App.css";
 import axios from 'axios';
 
@@ -7,35 +7,34 @@ import { SmurfContext } from '../context/smurfcontext';
 
 
 //components
-// import SmurfForm from './smurfform';
+import SmurfForm from './smurfform';
 import SmurfList from './smurflist';
 
-class App extends Component {
+class App extends React.Component {
 
   constructor() {
     super();
-    this.state = {
+    this.state ={
       smurfs:[],
     }
   }
+  componentDidMount(){
+    axios
+        .get('http://localhost:3333/smurfs')
+        .then((response) => {
+          //console.log(response.data,"response here")
+          const smurfsList = response.data
+          this.setState({smurfs: smurfsList});
+        })
+        .catch((err) => (err));
+    };
 
-componentDidMount(){
-  axios
-      .get('http://localhost:3333/smurfs')
-      .then(res => {
-        const smurfList = res.data
-        this.setState({smurfs: smurfList});
-      })
-      .catch((err) => (err));
-  };
-
-  render() {
+  render(){
     return (
       <SmurfContext.Provider value={this.state.smurfs}>
-             <div className="App">
-        {/* <SmurfForm /> */}
+        <h1>Welcome to Smurf Village</h1>
         <SmurfList />
-      </div>
+        <SmurfForm />
       </SmurfContext.Provider>
  
     );
--- smurfs/src/components/smurflist.js
+++ smurfs/src/components/smurflist.js
@@ -1,19 +1,20 @@
 import React, { useContext } from 'react';
 import { SmurfContext } from '../context/smurfcontext';
+// import Smurf from './smurf'
 
 
-const SmurfList = () => {
+const SmurfList = () =>{    
+    
     const smurfs = useContext(SmurfContext);
 
     return (
         <div className="smurf-list">
+            <>
             {smurfs.map(char => (
-                <div className="smurf-card">
-                    <h2 key={char.id}>
-                        Why, Hello there!I'm {char.name}. Some fun facts about me are that I am {char.age} years old and yet I am only {char.height} tall! What about you?!
-                    </h2>
-                </div>
-            ))}
+                <h3 key ={char.key}>Hey There! My name is {char.name}, I am {char.age} years old and I am only {char.height} tal!</h3>
+            ))}            
+            </>
+
         </div>
     );
 };
