b9fcdc8b9d9da853ff21e9980f2356074c09d070
Michelle Hey <miscmiche@gmail.com>
2020-06-26 19:49:37 -0400

    Smurf form complete for adding smurf, action addSmurf completed as well.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+
 import Smurf from "./Smurf";
 import SmurfForm from "./SmurfForm";
+
 import "./App.css";
 
 
@@ -8,6 +10,8 @@ import "./App.css";
 class App extends Component {
   render() {
     return (
+      <div className="App">
+        <h1>Smurf's 2.0 with Redux</h1>
           <Smurf/>
           <SmurfForm />
       </div>
--- smurfs/src/components/SmurfForm.js
+++ smurfs/src/components/SmurfForm.js
@@ -1,25 +1,84 @@
-import React from React;
+import React from 'react';
+import { connect } from 'react-redux';
 
-function SmurfForm() {
-  return (
-    <div className="form">
-      <form>
-        <input 
-          type='text'
-          name='name'
-          placeholder='name'
-        />
-        <input 
-          type='text'
-          name='age'
-          placeholder='age'
-        />
-        <input 
-          type='text'
-          name='height'
-          placeholder='height'
-        />
+import { addSmurf } from '../actions/addSmurf';
+
+console.log('addSmurf AC', addSmurf)
+
+class SmurfForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      age: 1,
+      height: 1
+    }
+  }
+
+  handleChange = event => {
+    this.setState({[event.target.name] : event.target.value})
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.addSmurf({ 
+      id: this.props.state.smurfs.length + 1,
+      name: this.state.name, 
+      age: this.state.age,
+      height: this.state.height 
+    })
+    this.setState({ name: '', age: 1, height: 1 })
+  }
+
+  render() {
+    console.log('state, props', this.state, this.props)
+    return (
+      <div>
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name
+          <input 
+            type='text'
+            name='name'
+            placeholder='name'
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <label>
+          Age (years)
+          <input 
+            type='text'
+            name='age'
+            placeholder='age'
+            value={this.state.age}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <label>
+          Height (cm)
+          <input 
+            type='text'
+            name='height'
+            placeholder='height'
+            value={this.state.height}
+            onChange={this.handleChange}
+          />
+        </label>
+
+        <button type='submit'>Submit</button>
       </form>
     </div>
-  )
-} 
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    state
+  }
+}
+
+export default connect(mapStateToProps, { addSmurf })(SmurfForm)
