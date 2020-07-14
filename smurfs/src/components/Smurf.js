6c8d242dbe6241c9ce25310828d37000084198f8
dipeshkoirala <dipeshkoirala@gmail.com>
2020-07-13 20:28:26 -0400

    finalizing settings

--- smurfs/src/components/Smurf.js
+++ smurfs/src/components/Smurf.js
@@ -4,8 +4,8 @@ const Smurf = ({ name, age, height }) => {
   return (
     <div className="dk3">
       <h2 className="dk1">{name}</h2>
-      <p>Age:{age} years old</p>
-      <p>Height:{height} cms</p>
+      <p className="dk1">Age:{age} years old</p>
+      <p className="dk1">Height:{height} cms</p>
     </div>
   );
 };
